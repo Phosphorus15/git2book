6c99cba620f351a6cbd6b77168d98a46900f34e3
Phosphorus15 <steepout@qq.com>
2018-08-26 20:19:35 +0800

    Initial Github Interface

--- src/main/java/GitInterface.kt
+++ src/main/java/GitInterface.kt
@@ -1,13 +1,15 @@
+import org.kohsuke.github.GHContent
+import org.kohsuke.github.GHRepository
 import org.kohsuke.github.GitHub
 
 class GitInterface(private val github: GitHub) {
 
     fun gatherRepo(repo: String): Repository =
             github.getRepository(repo).run {
-        RepositoryImpl(name, listLanguages().keys.toList()).apply {
+                GHRepositoryImpl(this).apply {
 
-        }
-    }
+                }
+            }
 
     fun gatherRepo(user: String, id: String) = gatherRepo("$user/$id")
 
@@ -19,12 +21,54 @@ abstract class Repository {
 
     abstract val languages: List<String>
 
+    abstract val rootEntry: Entry
+
     override fun toString(): String {
         return "$name / $languages"
     }
 
 }
 
-private class RepositoryImpl(override val name: String, override val languages: List<String>) : Repository() {
+abstract class Entry {
+
+    abstract val name: String
+
+    abstract val isDirectory: Boolean
+
+    abstract fun listEntries(): List<Entry>
+
+}
+
+class GHEntryImpl(private val content: GHContent) : Entry() {
+    override val name: String = content.name
+
+    override val isDirectory: Boolean = content.isDirectory
+
+    override fun listEntries(): List<Entry> =
+            if (content.isDirectory) content.listDirectoryContent().map { GHEntryImpl(it) }
+            else throw RuntimeException("Illegal access")
+
+}
+
+class VirtualGHRoot(private val repo: GHRepository) : Entry() {
+    override val name: String = "/"
+    override val isDirectory: Boolean = true
+
+    override fun listEntries(): List<Entry> = repo.getDirectoryContent("/").map { GHEntryImpl(it) }
+
+}
+
+private class GHRepositoryImpl(repo: GHRepository) : Repository() {
+    override val name: String = repo.name
+    override val languages: List<String> = repo.listLanguages().keys.toList()
+    override val rootEntry: Entry
+
+    init {
+        rootEntry = VirtualGHRoot(repo)
+    }
+
+}
+
+fun main(args: Array<String>) {
 
 }
